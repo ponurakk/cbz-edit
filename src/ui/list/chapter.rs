d88f739443add3790576a4481bc90147eda0b01a
ponurakk <p0unrakg@int.pl>
2026-02-07 01:03:34 +0100

    refactor: Move chapter/series to separate files

---
+++ src/ui/list/chapter.rs
@@ -0,0 +1,128 @@
+use std::{collections::HashSet, path::PathBuf};
+
+use ratatui::widgets::{ListState, ScrollbarState};
+
+/// Chapter of a series from disk
+#[derive(Debug, Clone)]
+pub struct Chapter {
+    /// Path to the chapter (cbz file)
+    pub path: PathBuf,
+
+    /// Volume of the chapter
+    pub volume: Option<u32>,
+
+    /// Chapter number
+    #[allow(clippy::struct_field_names)]
+    pub chapter: Option<f32>,
+
+    /// Title of the chapter
+    pub title: Option<String>,
+
+    /// Translators
+    pub translators: Vec<String>,
+}
+
+impl Default for Chapter {
+    fn default() -> Self {
+        Self {
+            path: PathBuf::new(),
+            volume: None,
+            chapter: None,
+            title: None,
+            translators: vec![],
+        }
+    }
+}
+
+impl PartialEq for Chapter {
+    fn eq(&self, other: &Self) -> bool {
+        self.volume == other.volume && self.chapter == other.chapter
+    }
+}
+impl Eq for Chapter {}
+
+impl PartialOrd for Chapter {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Chapter {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let c1 = self.chapter.unwrap_or(0.0);
+        let c2 = other.chapter.unwrap_or(0.0);
+        match c1.partial_cmp(&c2).unwrap_or(std::cmp::Ordering::Equal) {
+            std::cmp::Ordering::Equal => self.path.cmp(&other.path),
+            non_eq => non_eq,
+        }
+    }
+}
+
+/// List of chapters in a series
+#[derive(Debug, Clone, Default)]
+pub struct ChapterList {
+    /// Shown state of projects
+    pub items: Vec<Chapter>,
+
+    /// Initial static state of projects
+    pub items_state: Vec<Chapter>,
+
+    /// State of the list
+    pub state: ListState,
+
+    /// State of the scrollbar
+    pub scroll_state: ScrollbarState,
+
+    /// Custom field to track multiple selections
+    pub selected: HashSet<usize>,
+}
+
+impl FromIterator<Chapter> for ChapterList {
+    fn from_iter<T: IntoIterator<Item = Chapter>>(iter: T) -> Self {
+        let mut state = ListState::default();
+        state.select_first();
+
+        let items: Vec<Chapter> = iter.into_iter().collect();
+        let len = items.len();
+        Self {
+            items: items.clone(),
+            items_state: items,
+            state,
+            scroll_state: ScrollbarState::default().content_length(len),
+            selected: HashSet::new(),
+        }
+    }
+}
+
+impl ChapterList {
+    pub fn toggle_selected(&mut self) {
+        if let Some(index) = self.state.selected()
+            && !self.selected.insert(index)
+        {
+            self.selected.remove(&index);
+        }
+    }
+}
+
+impl Chapter {
+    pub fn get_title(&self, selected: bool) -> String {
+        let selected_char = if selected {
+            String::from("▌")
+        } else {
+            String::from(" ")
+        };
+
+        format!(
+            "{}{:#5.}: {}",
+            selected_char,
+            self.chapter.unwrap_or_default(),
+            self.title.clone().unwrap_or(
+                self.path
+                    .file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string(),
+            )
+        )
+    }
+}
---
+++ src/ui/list/mod.rs
@@ -0,0 +1,5 @@
+mod chapter;
+mod series;
+
+pub use chapter::{Chapter, ChapterList};
+pub use series::{Series, SeriesList};
